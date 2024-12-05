9739e20769d1e89e0b3a6b1919c384737490d334
Alejandro Castellanos <112025060+Tallikarcz@users.noreply.github.com>
2024-12-04 21:25:57 -0300

    descripciones
    xd

--- js/script.js
+++ js/script.js
@@ -1,74 +1,96 @@
 // EDITOR DE DOM DATOS POKEMON
-const pokemonNameAndNumber = document.querySelector(' .pokemon__name');
-const pokemonNumber = document.querySelector(' .pokemon__number');
-const pokemonImage = document.querySelector(' .pokemon__image');
-const pokemonWeight = document.querySelector(' .pokemon__weight');
-const pokemonHeight = document.querySelector(' .pokemon__height');
-const pokemonType = document.querySelector(' .pokemon__type');
-
-const form = document.querySelector(' .form');
-const input = document.querySelector(' .input__search');
+const pokemonNameAndNumber = document.querySelector(" .pokemon__name");
+const pokemonNumber = document.querySelector(" .pokemon__number");
+const pokemonImage = document.querySelector(" .pokemon__image");
+const pokemonWeight = document.querySelector(" .pokemon__weight");
+const pokemonHeight = document.querySelector(" .pokemon__height");
+const pokemonType = document.querySelector(" .pokemon__type");
+const pokemonDesc = document.querySelector(" .pokemon__desc")
+const form = document.querySelector(" .form");
+const input = document.querySelector(" .input__search");
 
 // BOTONES
-const buttonPrev = document.querySelector(' .btn-prev');
-const buttonNext = document.querySelector(' .btn-next');
+const buttonPrev = document.querySelector(" .btn-prev");
+const buttonNext = document.querySelector(" .btn-next");
 
 // INICIAR POKEDEX EN POKEMON 1
 let searchPokemon = 1;
 
+// FUNCION ASINCRONA DATA POKEMON
 const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+};
+// FUNCION ASINCRONA DATA CON DESCRIPCION POKEMON
+const fetchPokemonDesc = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${pokemon}`
+  );
 
-    if (APIResponse.status === 200) {
-        const data = await APIResponse.json();
-        return data;
-    }
+  if (APIResponse.status === 200) {
+    const dataDesc = await APIResponse.json();
+    return dataDesc;
+  }
 }
 
+// RENDER POKEMON
 const renderPokemon = async (pokemon) => {
+  pokemonNameAndNumber.innerHTML = "Loading... ";
+  pokemonWeight.innerHTML = " ";
+  pokemonHeight.innerHTML = " ";
+  pokemonType.innerHTML = " ";
+  pokemonDesc.innerHTML = " ";
 
-    pokemonNameAndNumber.innerHTML = 'Loading... ';
-    pokemonWeight.innerHTML = ' ';
-    pokemonHeight.innerHTML = ' ';
-    pokemonType.innerHTML = ' ';
+  const data = await fetchPokemon(pokemon);
+  const dataDesc = await fetchPokemonDesc(pokemon);  
+  console.log(dataDesc);
 
-    const data = await fetchPokemon(pokemon)
-    console.log(data);
-    
-    if (data) {
-        pokemonImage.style.display = 'block';
-        pokemonNameAndNumber.innerHTML = data.id + " - " + data.name;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-        pokemonWeight.innerHTML = (data.weight)/10 + " KG";
-        pokemonHeight.innerHTML = (data.height)/10 + " meters";
-        pokemonType.innerHTML = "Type: " + " " + data.types[0].type.name;
-        input.value = '';
-        searchPokemon = data.id;
-        const cries = new Audio(data.cries?.latest || data.cries?.legacy || '');
-        cries.play();
-    } else {
-        pokemonImage.style.display = 'none';
-        pokemonNameAndNumber.innerHTML = 'Not found!';
-    }
-}
+  if (data) {
+    pokemonImage.style.display = "block";
+    pokemonNameAndNumber.innerHTML = data.id + " - " + data.name;
+    pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"] || data["sprites"]["front_default"]; // SI NO TIENE SPRITE ANIMADO, APARECE SPRITE ESTATICO
+    pokemonWeight.innerHTML = data.weight / 10 + " KG";
+    pokemonHeight.innerHTML = data.height / 10 + " meters";
+    pokemonType.innerHTML = "Type: " + " " + data.types[0].type.name;  
+    const descInEnglish = dataDesc.flavor_text_entries.find(entry => entry.language.name === "en");
+    pokemonDesc.innerHTML = descInEnglish ? descInEnglish.flavor_text : "No description available";
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    renderPokemon(input.value.toLowerCase())
+    input.value = "";
+    searchPokemon = data.id;
+
+    // SONIDOS DEL POKEMON
+    const cries = new Audio(data.cries?.latest || data.cries?.legacy || "");
+    cries.play();
+
+  } else {
+    pokemonImage.style.display = "none";
+    pokemonNameAndNumber.innerHTML = "Not found!";
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
 });
 
-buttonPrev.addEventListener('click', () => {
-
-    if (searchPokemon > 1) {
-        searchPokemon-=1;
-        renderPokemon(searchPokemon)
-    }
+buttonPrev.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
 });
 
-buttonNext.addEventListener('click', () => {
-    searchPokemon+=1;
-    renderPokemon(searchPokemon)
+buttonNext.addEventListener("click", () => {
+  if (searchPokemon < 1025) {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  }
 });
 
 renderPokemon(searchPokemon);
