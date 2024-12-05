04aabce2db2c5be71143b94817cabf9c16bc531c
Alejandro Castellanos <112025060+Tallikarcz@users.noreply.github.com>
2024-12-04 22:43:56 -0300

    boton de shiny xd

--- js/script.js
+++ js/script.js
@@ -9,9 +9,13 @@ const pokemonDesc = document.querySelector(" .pokemon__desc")
 const form = document.querySelector(" .form");
 const input = document.querySelector(" .input__search");
 
+// Valor por defecto sprite
+let defaultSprite = true;
+
 // BOTONES
 const buttonPrev = document.querySelector(" .btn-prev");
 const buttonNext = document.querySelector(" .btn-next");
+const buttonShiny = document.querySelector(" .btn-shiny");
 
 // INICIAR POKEDEX EN POKEMON 1
 let searchPokemon = 1;
@@ -54,7 +58,7 @@ const renderPokemon = async (pokemon) => {
   if (data) {
     pokemonImage.style.display = "block";
     pokemonNameAndNumber.innerHTML = data.id + " - " + data.name;
-    pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"] || data["sprites"]["front_default"]; // SI NO TIENE SPRITE ANIMADO, APARECE SPRITE ESTATICO
+
     pokemonWeight.innerHTML = data.weight / 10 + " KG";
     pokemonHeight.innerHTML = data.height / 10 + " meters";
     pokemonType.innerHTML = "Type: " + " " + data.types[0].type.name;  
@@ -63,6 +67,22 @@ const renderPokemon = async (pokemon) => {
 
     input.value = "";
     searchPokemon = data.id;
+    // Sprite por defecto
+    pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"] || data["sprites"]["front_default"];
+
+    // Funcion boton shiny
+    function toggleSprite() {
+  
+      if (defaultSprite) {
+        pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_shiny"];
+        defaultSprite = false; 
+      } else {
+        pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"] || data["sprites"]["front_default"];
+        defaultSprite = true; 
+      }
+    }
+
+    buttonShiny.onclick = toggleSprite;
 
     // SONIDOS DEL POKEMON
     const cries = new Audio(data.cries?.latest || data.cries?.legacy || "");
